602bc732183b70d3f421f1d18c3b64159a3bb02a
Madu-Montes <monteseduarda56@gmail.com>
2024-01-04 21:45:18 -0300

    salvando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mouse = document.querySelector('.mouse');
 const trash = document.querySelector('.trash');
+const gameover = document.querySelector('.gameover');
 
 const jump = () => {
     mouse.classList.add('jump');
@@ -11,12 +12,29 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-    const trashPosition = trash.offsetLeft;
+    console.log('loop')
 
-    if (trashPosition <= 135) {
+    const trashPosition = trash.offsetLeft;
+    const mousePosition = +window.getComputedStyle(mouse).bottom.replace('px', '');
+
+    console.log(mousePosition);
+
+    if (trashPosition <= 135 && trashPosition > 0 && mousePosition < 80) {
 
       trash.style.animation = 'none';
       trash.style.left = '${trashPosition}px';
+
+      mouse.style.animation = 'none';
+      mouse.style.bottom = '${mousePosition}px';
+
+      mouse.src = './images/mousedead.png'
+      mouse.style.width = '130px'
+      mouse.style.bottom = '-20px'
+      mouse.style.marginLeft = '50px'
+
+      gameover.style.display = 'block';
+
+      clearInterval(loop);
         
     }
 
