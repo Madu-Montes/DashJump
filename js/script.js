2546466bea9b5c0359f8e03f239c78b443d70149
Madu-Montes <monteseduarda56@gmail.com>
2024-01-08 20:42:47 -0300

    alteração nas ações

--- js/script.js
+++ js/script.js
@@ -21,23 +21,31 @@ const loop = setInterval(() => {
 
     if (villainPosition <= 135 && villainPosition > 0 && sonicPosition < 80) {
 
-      villain.style.animation = 'none';
-      villain.style.left = '${villainPosition}px';
+        villain.style.animation = 'none';
+        villain.style.left = '${villainPosition}px';
 
-      sonic.style.animation = 'none';
-      sonic.style.bottom = '${sonicPosition}px';
+        sonic.style.animation = 'none';
+        sonic.style.bottom = '${sonicPosition}px';
 
-      sonic.src = './images/sonicover.gif'
-      sonic.style.width = '250px'
-      sonic.style.bottom = '-40px'
-      sonic.style.marginLeft = '0px'
+        sonic.src = './images/sonicover.gif'
+        sonic.style.width = '250px'
+        sonic.style.bottom = '-40px'
+        sonic.style.marginLeft = '0px'
 
-      gameover.style.display = 'block';
+        gameover.style.display = 'block';
 
-      clearInterval(loop);
-        
+        clearInterval(loop);
+
     }
 
 }, 10)
 
-document.addEventListener('keydown', jump); 
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 38) {
+        jump();
+    }
+});
+
+document.addEventListener('touchstart', () => {
+    jump();
+});
